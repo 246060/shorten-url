ba8e07c6f919839c82a08852fce5e737c456ad00
blackcocoon <42939960+blackcocoon@users.noreply.github.com>
2022-02-04 08:20:13 +0900

    test: add sql schema file and data file

---
+++ db/data.sql
@@ -0,0 +1,15 @@
+-- User 데이터 생성
+INSERT INTO user(email, `name`, provider_type, provider_uid, `role`, created_at, updated_at, created_by, updated_by)
+VALUES ('web-master@jocn.xyz', 'WEB-MASTER', '', '', 'SYSTEM', now(), now(), 1, 1);
+INSERT INTO user(email, `name`, provider_type, provider_uid, `role`, created_at, updated_at, created_by, updated_by)
+VALUES ('batch01@jocn.xyz', 'BATCH01', '', '', 'SYSTEM', now(), now(), 1, 1);
+INSERT INTO user(email, `name`, provider_type, provider_uid, `role`, created_at, updated_at, created_by, updated_by)
+VALUES ('notification@jocn.xyz', 'NOTIFICATION', '', '', 'SYSTEM', now(), now(), 1, 1);
+INSERT INTO user(email, `name`, provider_type, provider_uid, `role`, created_at, updated_at, created_by, updated_by)
+VALUES ('recruit@jocn.xyz', 'HR', '', '', 'ADMIN', now(), now(), 1, 1);
+--
+
+
+
+
+
---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP TABLE IF EXISTS `user` CASCADE;
+DROP TABLE IF EXISTS `url` CASCADE;
+
+CREATE TABLE `user` (
+    `uid` bigint(20) NOT NULL AUTO_INCREMENT,
+    `email` varchar(50) NOT NULL,
+    `name` varchar(50) NOT NULL,
+    `picture` varchar(400) DEFAULT NULL,
+    `provider_type` varchar(20) NOT NULL,
+    `provider_uid` varchar(100) NOT NULL,
+    `role` varchar(20) NOT NULL,
+    `created_at` datetime(6),
+    `updated_at` datetime(6),
+    PRIMARY KEY (`uid`),
+    UNIQUE KEY `uk_user_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `url` (
+   `url_id` bigint(20) NOT NULL AUTO_INCREMENT,
+   `click_cnt` int(11) NOT NULL,
+   `created_at` datetime(6) NOT NULL,
+   `origin_url` varchar(3000) DEFAULT NULL,
+   `shorten_url` varchar(40) DEFAULT NULL,
+   `state` varchar(20) DEFAULT NULL,
+   `updated_at` datetime(6) NOT NULL,
+   `created_by` bigint(20) DEFAULT NULL,
+   `updated_by` bigint(20) DEFAULT NULL,
+   PRIMARY KEY (`url_id`),
+   KEY `fk_user_url_01` (`created_by`),
+   KEY `fk_user_url_02` (`updated_by`),
+   CONSTRAINT `fk_user_url_01` FOREIGN KEY (`created_by`) REFERENCES `user` (`uid`),
+   CONSTRAINT `fk_user_url_02` FOREIGN KEY (`updated_by`) REFERENCES `user` (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=100000000000000000 DEFAULT CHARSET=utf8mb4;
